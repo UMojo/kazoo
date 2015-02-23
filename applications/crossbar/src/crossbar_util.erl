5dee40175c7d2e6022fb5195e627273bb6bfdf17
Pierre Fenoll <pierrefenoll@gmail.com>
2015-02-23 11:15:19 -0800

    debugcode

--- applications/crossbar/src/crossbar_util.erl
+++ applications/crossbar/src/crossbar_util.erl
@@ -108,6 +108,7 @@ response_400(Message, Data, Context) ->
 -spec response_402(wh_json:object(), cb_context:context()) ->
                           cb_context:context().
 response_402(Data, Context) ->
+    io:format(">>> Data = ~p\nContext = ~p\nST = ~p\n", [Data,Context,erlang:get_stacktrace()]),
     create_response('error', <<"accept charges">>, 402, Data, Context).
 
 %%--------------------------------------------------------------------
